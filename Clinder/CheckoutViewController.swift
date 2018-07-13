5fd77d3d5fffeb6183336475b629dc34efdd04b9
ktjlee <ktjlee@umich.edu>
2018-07-12 18:12:49 -0700

    design

---
+++ Clinder/CheckoutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CheckoutViewController.swift
+//  Clinder
+//
+//  Created by Katie  Lee on 7/12/18.
+//  Copyright © 2018 Katie  Lee. All rights reserved.
+//
+
+import UIKit
+
+class CheckoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ Clinder/customCell.swift
@@ -0,0 +1,24 @@
+//
+//  customCell.swift
+//  Clinder
+//
+//  Created by Katie  Lee on 7/12/18.
+//  Copyright © 2018 Katie  Lee. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
